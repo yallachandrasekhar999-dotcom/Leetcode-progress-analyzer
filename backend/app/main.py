acf5ebc4f5a2ebe234ee594cf40ab11ad6d911c8
chandu <yallachandrasekhar999@gmail.com>
2026-06-25 20:01:47 +0530

    Add gunicorn

--- backend/app/main.py
+++ backend/app/main.py
@@ -16,7 +16,7 @@ app = FastAPI(
 # CORS for React frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "*"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000","https://YOUR-FRONTEND.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
